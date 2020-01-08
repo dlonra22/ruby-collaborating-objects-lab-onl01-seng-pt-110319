ab5723d6707a3bad41696cfe067eccee53cf5a17
arnie b <arnold.bwaila@gmail.com>
2020-01-08 00:54:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,7 @@ class Song
   def self.new_by_filename(filename)
     song_and_art = filename.split(" - ")
     song = self.new(song_and_art[1])
-    artist_name = song_and_art[0]
+    self.artist_name = song_and_art[0]
     @@all<< song
     song
   end
